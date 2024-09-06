3083ed5ebacf69605faf6d3bb9e955dd0907ad9e
BryaN McLean <bmclean2025@jpkeefehs.org>
2024-09-06 11:54:15 -0400

    phot gal and hotle

--- testing.js
+++ testing.js
@@ -1,11 +1,22 @@
 
 
 
-function setTime() {
+function setVegasTime() {
     var vegasTime = new Intl.DateTimeFormat('en-US', { timeZone: 'America/Los_Angeles', timeStyle: 'full', dateStyle: 'short' }).format(new Date());
     document.getElementById('vegas-time').innerHTML = "Current Time/Date : " + vegasTime;
-
 }
+
+function setDetroitTime() {
+    var vegasTime = new Intl.DateTimeFormat('en-US', { timeZone: 'America/Detroit', timeStyle: 'full', dateStyle: 'short' }).format(new Date());
+    document.getElementById('detroit-time').innerHTML = "Current Time/Date : " + vegasTime;
+}
+
+
+function setPennTime() {
+    var vegasTime = new Intl.DateTimeFormat('en-US', { timeZone: 'America/New_York', timeStyle: 'full', dateStyle: 'short' }).format(new Date());
+    document.getElementById('penn-time').innerHTML = "Current Time/Date : " + vegasTime;
+}
+
 
 function checkEmail() {
     email = document.getElementById('emailInput').value;
@@ -14,7 +25,6 @@ function checkEmail() {
 
     var valid = tester.test(email);
     if(!valid){
-        console.log(false);
         document.getElementById('checkedEmail').innerHTML = "Please enter email - Invalid Email Address";
         return false;
     }
@@ -22,16 +32,13 @@ function checkEmail() {
     var emailParts = email.split('@');
 
     if(emailParts[0].length >64){
-        console.log("false");
         document.getElementById('checkedEmail').innerHTML = "Please enter email - Invalid Email Address";
     }
     var domainParts = emailParts[1].split(".");
     if(domainParts.some(function(part) { return part.length > 63})){
-        console.log("false");
         document.getElementById('checkedEmail').innerHTML = "Please enter email - Invalid Email Address";
     }
   
-    console.log('sucess');
     document.getElementById('checkedEmail').innerHTML = "Please enter email - Succesfully Added you to the Newsletter!";
     return true;
   };
